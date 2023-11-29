91e6cb4f54d39bafe397951eb5f7bd191eb98a2b
TouchingFishes <lutzonino03@gmail.com>
2023-11-29 15:21:38 +0100

    Reverted Grass back to vanilla

--- data/wild/johto_grass.asm
+++ data/wild/johto_grass.asm
@@ -1238,17 +1238,17 @@ JohtoGrassWildMons:
 	db 10 percent, 10 percent, 10 percent ; encounter rates: morn/day/nite
 	; morn
 	dbw 2, PIDGEY
-	dbw 2, CORSOLA
-	dbw 3, CORSOLA
-	dbw 3, CORSOLA
-	dbw 2, CORSOLA
+	dbw 2, SENTRET
+	dbw 3, PIDGEY
+	dbw 3, SENTRET
+	dbw 2, RATTATA
 	dbw 3, HOPPIP
 	dbw 3, HOPPIP
 	; day
 	dbw 2, PIDGEY
-	dbw 2, CORSOLA
-	dbw 3, CORSOLA
-	dbw 3, CORSOLA
+	dbw 2, SENTRET
+	dbw 3, PIDGEY
+	dbw 3, SENTRET
 	dbw 2, RATTATA
 	dbw 3, HOPPIP
 	dbw 3, HOPPIP
